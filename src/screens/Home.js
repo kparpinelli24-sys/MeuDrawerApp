9613d3e8b584c033397075e73c3b3d7e79397412
kparpinelli24-sys <k.parpinelli.24@gmail.com>
2026-05-26 09:31:16 -0300

    Fix Home screen text and add Profile screen component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,7 +3,7 @@ import{ view, Text } from 'react-native';
 export default function Home() {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Inicio</Text>
+      <Text>Home</Text>
     </View>
   );
 }
